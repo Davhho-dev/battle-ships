f0700da7c4ab820c7a0b42ba3773c284e5b634fb
Davhho-dev <Davhho@utexas.edu>
2023-06-06 20:47:52 -0500

    Create gameboard module and function to create gameboard

---
+++ src/gameboard.js
@@ -0,0 +1,17 @@
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+
+function createGameBoard(elem, position) {
+    for(let col = 0; col < 10; col++) {
+        const column = document.createElement("div");
+        column.className = `col-${letters[col]}`;
+        elem.appendChild(column)
+        for(let row = 1; row <= 10; row++) {
+            const sq = document.createElement("div");
+            sq.className = `sq-${letters[col]}${row}`;
+            sq.textContent = `${letters[col]}${row}`;
+            column.appendChild(sq);
+        }
+    }
+}
+
+export {createGameBoard};
